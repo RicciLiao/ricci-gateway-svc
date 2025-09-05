8170ef13ca8d82b9e69ac0c34da0170cf7f21ae7
ricciliao <riccix@163.com>
2025-09-05 15:47:09 +0800

    refactor: code review.

--- src/main/java/ricciliao/gateway/filter/GatewayGlobalFilter.java
+++ src/main/java/ricciliao/gateway/filter/GatewayGlobalFilter.java
@@ -1,17 +1,17 @@
 package ricciliao.gateway.filter;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+import ricciliao.x.log.AuditLoggerFactory;
+import ricciliao.x.log.logger.AuditLogger;
 
 public class GatewayGlobalFilter implements GlobalFilter, Ordered {
 
-    private static final Logger logger = LoggerFactory.getLogger(GatewayGlobalFilter.class);
+    private static final AuditLogger logger = AuditLoggerFactory.getLogger(GatewayGlobalFilter.class);
 
     private final Integer order;
 
